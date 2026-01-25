e53b8960b1a0e3f63c13e1c0a5f5929bdb59e3ea
יהונתן אוחנה <yehonatan2210@gmail.com>
2026-01-25 10:42:41 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,11 +1,9 @@
 import Home from './pages/Home';
-import Static from './pages/Static';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "Home": Home,
-    "Static": Static,
 }
 
 export const pagesConfig = {
